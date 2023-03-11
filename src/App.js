34ddc998450539123103586584092da3e9b4352e
Sticx <108312079+Sticx@users.noreply.github.com>
2023-03-11 17:56:13 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Main from './components/Main/Main';
+import { v4 as idGenerator } from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todoList: [],
+            inputValue: '',
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleDone = this.handleDone.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({ inputValue: e.target.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        if (this.state.inputValue) {
+            const newItem = {
+                text: this.state.inputValue,
+                id: idGenerator(),
+                done: false,
+            };
+            this.setState({
+                todoList: [newItem, ...this.state.todoList],
+                inputValue: '',
+            });
+        }
+    }
+
+    handleDone(e) {
+        const todoList = [...this.state.todoList];
+        todoList.map(element => {
+            if (e.id === element.id) {
+                element.done = !element.done;
+            }
+            return element;
+        });
+        this.setState({ todoList });
+    }
+
+    render() {
+        return (
+            <div>
+                <div className='wrapper'>
+                    <Main state={this.state}
+                          handleChange={this.handleChange}
+                          handleSubmit={this.handleSubmit}
+                          handleDone={this.handleDone}
+                    />
+                </div>
+            </div>
+        );
+    }
+
 }
 
 export default App;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Main.css';
+import TodoForm from './TodoForm/TodoForm';
+
+class Main extends React.Component {
+
+    render() {
+        return (
+            <main className='main'>
+                <h1 className='main__title'>Awesome TODO List</h1>
+                <TodoForm state={this.props.state}
+                          handleChange={this.props.handleChange}
+                          handleSubmit={this.props.handleSubmit}
+                          handleDone={this.props.handleDone}
+                />
+            </main>
+        );
+    }
+}
+
+export default Main;
